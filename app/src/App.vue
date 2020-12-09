a772d78d6f8586831f502c1ff8173c94a47f7317
Jeremy Tandjung <jeremytandjung@icloud.com>
2020-12-09 15:01:14 -0800

    started on expanding cards

---
+++ app/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="main-container">
+    <card-one
+      title="Grizz"
+      imgLink="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.v8-QAbCvzLeW61kDqjDmMAHaFZ%26pid%3DApi&f=1"
+    />
+
+    <card-one
+      title="Panda"
+      imgLink="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.vYmmng-wR4t5upJhILvfmAAAAA%26pid%3DApi&f=1"
+    ></card-one>
+
+    <card-one
+      title="Ice Bear"
+      imgLink="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2F4X_aIGXtKJY%2Fmaxresdefault.jpg&f=1&nofb=1"
+    />
+  </div>
+</template>
+
+<script>
+import CardOne from './components/CardOne';
+
+export default {
+  name: 'App',
+  components: {
+    CardOne
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #490f25;
+}
+
+main-container {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 30px;
+  width: 1100px;
+}
+
+</style>
---
+++ app/src/components/CardOne.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="card">
+        <div class="imgBox">
+            <img :src="imgLink">
+        </div>
+        <div class="content">
+            <h2>{{ title }}</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora aut deleniti dolorum natus tempore dolores! Quaerat minus dolores fugiat optio tempore ut in debitis voluptates quisquam! Nobis modi optio natus.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CardOne',
+
+    props: {
+        title: String,
+        imgLink: String
+    },
+
+    data () {
+        return {
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+    position: relative;
+    max-width: 300px;
+    height: 215px;
+    background: white;
+    margin: 30px 10px;
+    padding: 20px 15px;
+    display: flex;
+    flex-direction: column;
+    box-shadow: 0 5px 20px rgba(0, 0, 0, 0.5);
+    transition: 0.5s ease-in-out;
+}
+
+.card:hover {
+    height: 420px;
+}
+
+.imgBox {
+    position: relative;
+    width: 260px;
+    height: 260px;
+    top: -60px;
+    left: 20px;
+    z-index: 1;
+    box-shadow: 0 5px 202px rgba(0, 0, 0, 0.5);
+}
+
+img {
+    max-width: 100%;
+    border-radius: 8px;
+}
+
+.content {
+    position: relative;
+    margin-top: -140px;
+    padding: 10px 15px;
+    text-align: center;
+    color: #111;
+    visibility: hidden;
+}
+
+.card:hover .content{
+    visibility: visible;
+    opacity: 1;
+    margin-top: -40px;
+    transition-delay: 0.3x;
+}
+</style>
